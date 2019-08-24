f1ca1abf1904a898c473145c38cc00698fcd6424
Eric Diaz <ericdiaz@pursuit.org>
2019-08-24 14:41:50 -0400

    created FizzBuzz Activity

---
+++ app/src/main/java/ericdiaz/program/mvptestapp/view/FizzBuzzActivity.java
@@ -0,0 +1,16 @@
+package ericdiaz.program.mvptestapp.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ericdiaz.program.mvptestapp.R;
+
+public class FizzBuzzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_fizz_buzz);
+    }
+}
